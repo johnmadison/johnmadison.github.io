37a00e8c33b672adcda104b44c674a63ce896a0b
John Madison <Madison.John.R@gmail.com>
2016-03-18 16:24:11 -0600

    sdfsdfsdf

--- search/search.js
+++ search/search.js
@@ -59,6 +59,7 @@ function search(data) {
     // Use the JavaScript client library to create a search.list() API call.
     
     term = data.Word;
+    alert(term);
     var request = gapi.client.youtube.search.list({
         part: 'snippet',
         q: term
