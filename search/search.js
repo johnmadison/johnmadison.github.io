dc78a51679026aea807e04c198281d36d02ced20
John Madison <Madison.John.R@gmail.com>
2016-03-18 17:36:29 -0600

    XDYTCFJKLN

--- search/search.js
+++ search/search.js
@@ -90,24 +90,24 @@ var tag = document.createElement('script');
 
       // 4. The API will call this function when the video player is ready.
       function onPlayerReady(event) {
-       if(event.data === 0) //state 0 is when a video ends
-            {          
+                 
             	
-            	RanddomWord();
+            	RandomWord();
                 
-            }
+            
       }
 
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
+     
       function onPlayerStateChange(event) {
+      if(event.data === 0) //state 0 is when a video ends
+            {          
+            	
+            	RandomWord();
+                
+            }
       
       }
-      function stopVideo() {
-        player.stopVideo();
-      }
+      
       
       
 
