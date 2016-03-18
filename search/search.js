8e052cb8e6bee0fed98e2ea111a56a9ade224ffd
John Madison <Madison.John.R@gmail.com>
2016-03-18 15:15:18 -0600

    sdfs

--- search/search.js
+++ search/search.js
@@ -38,7 +38,7 @@ function onClientLoad() {
 function onYouTubeApiLoad() {
     // This API key is intended for use only in this lesson.
     // See https://goo.gl/PdPA1 to get a key for your own applications.
-    gapi.client.setApiKey('AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE');
+    gapi.client.setApiKey('AIzaSyBwZOpcHtNfvtI0uzEYmcnrqyurX3Hiof8');
 
     search();
 }
